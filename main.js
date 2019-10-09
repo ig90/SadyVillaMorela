e4911b4c4a7db5c464bf625ca33757a7c164d0fd
Igor <igor.filipiuk@gmail.com>
2019-10-09 23:07:30 +0200

    a bit of refactoring

--- main.js
+++ main.js
@@ -8,22 +8,22 @@ const handleClick = () => {
   hamburger.classList.toggle('hamburger--active');
   hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
   nav.classList.toggle('navigation--active');
-
 }
 
 hamburger.addEventListener('click', handleClick);
 
+// hide the menu after clicking on the menu item
 nav_link.forEach((nav_link) => {
   nav_link.addEventListener('click', handleClick);
 });
-
 
 function showNavPanel() {
 
   let scroll = this.scrollY;
   let nav = document.querySelector('nav');
   let logo = document.querySelector('.logo');
-  if (scroll >= 100) {
+  let offset = 60;
+  if (scroll >= offset) {
     nav.classList.add('nav-panel');
     logo.style.display = 'block';
   } else {
@@ -33,7 +33,4 @@ function showNavPanel() {
 }
 
 window.addEventListener("scroll", showNavPanel);
-
-
-
 
