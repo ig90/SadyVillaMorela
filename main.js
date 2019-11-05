b690227bc60877694ecbe77d1103b82f9bf3e7ae
Igor <igor.filipiuk@gmail.com>
2019-11-05 17:24:59 +0100

    changed gallery to dynamic image loading

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 
-
-/* particles */
-particlesJS.load('particles-js', 'particlesjs-config.json', function() {
-  console.log('particles.js loaded');
-});
-
 /* menu */
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.navigation');
@@ -40,3 +34,60 @@ function showNavPanel() {
 
 window.addEventListener("scroll", showNavPanel);
 
+/* gallery */
+
+const gallery = document.querySelector('#gallery');
+
+const createGalleryHeader = (hName) => {
+
+  let h = document.createElement('h2');
+      h.innerHTML = hName;
+      gallery.appendChild(h);
+}
+
+const createGallery = (path, galleryName, hName) => {
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", path, true);
+  xhr.responseType = 'document';
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+
+      createGalleryHeader(hName);
+
+      let elements = xhr.response.getElementsByTagName("a");
+
+      for (let x of elements) {
+
+        if (x.href.match(/\.(jpe?g|png|webp)$/)) { 
+
+            let imgLink = document.createElement('a');
+            let img = new Image();
+            imgLink.appendChild(img);
+            imgLink.setAttribute('data-fslightbox', galleryName);
+            imgLink.setAttribute('href', '.'+ x.href.substr(x.href.indexOf('/images')));
+
+            img.src = '.' + x.href.substr(x.href.indexOf('/images'));
+            gallery.appendChild(imgLink);
+            updateFsLightbox();
+        } 
+      }
+    } 
+    else {
+      alert('Request failed. Returned status of ' + xhr.status);
+    }
+  }
+  xhr.send();
+}
+
+createGallery("./images/", "gallery", "");
+createGallery("./images/parter", "gallery-parter", "Parter");
+createGallery("./images/apartament", "gallery-apartament", "Apartament");
+createGallery("./images/pietro", "gallery-pietro", "Piętro");
+createGallery("./images/klub", "gallery-klub", "Klub");
+
+
+/* particles */
+particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+  console.log('particles.js loaded');
+});
